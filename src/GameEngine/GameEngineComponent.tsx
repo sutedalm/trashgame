2eaab07fc529048d29796c3b28048a2c0a015964
Sébastien Letzelter <sebastien.letzelter@gmail.com>
2021-11-20 18:27:01 +0100

    Add frame to game

--- src/GameEngine/GameEngineComponent.tsx
+++ src/GameEngine/GameEngineComponent.tsx
@@ -1,3 +1,4 @@
+import "./GameEngineComponent.scss";
 import { Component } from "react";
 import { TrashGame } from "./trash-game";
 
@@ -5,7 +6,20 @@ export class GameEngineComponent extends Component {
     game: TrashGame | undefined;
 
     render() {
-        return <canvas></canvas>;
+        return (
+            <div>
+                <canvas></canvas>
+                <div className="frame">
+                    <div className="sides"></div>
+                    <div className="bottom"></div>
+                    <div className="labels">
+                        <p>Residual Waste</p>
+                        <p>Yellow Bin</p>
+                        <p>Organic Garbage</p>
+                    </div>
+                </div>
+            </div>
+        );
     }
 
     componentDidMount() {
