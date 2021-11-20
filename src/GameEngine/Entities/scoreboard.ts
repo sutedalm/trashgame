733f1d8dc369c665b6bf0790922355c9d07b57c3
Sébastien Letzelter <sebastien.letzelter@gmail.com>
2021-11-20 20:03:14 +0100

    Move scoreboard to the right

--- src/GameEngine/Entities/scoreboard.ts
+++ src/GameEngine/Entities/scoreboard.ts
@@ -16,9 +16,9 @@ export class Scoreboard implements Entity {
     }
 
     render(display: Display): void {
-        display.drawText(10, 40, "score: " + this.score, "30px Arial", "black", false);
+        display.drawText(25, 40, "score: " + this.score, "30px Arial", "black", false);
         for (let i = 0; i < this.lifes; i++) {
-            display.drawImage(10 + 42 * i, 80, 32, 32, "heart.png");
+            display.drawImage(25 + 42 * i, 80, 32, 32, "heart.png");
         }
         if (this.lifes == 0) {
             display.drawText(
