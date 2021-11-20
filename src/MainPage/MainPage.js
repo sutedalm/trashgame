b7c8b51959928948331d09bfe57ce6ce65d36653
Maria Pospelova <mariyapospelova@gmail.com>
2021-11-20 14:20:55 +0100

    Add main page with link to game

---
+++ src/MainPage/MainPage.js
@@ -0,0 +1,36 @@
+import { GameEngineComponent } from "../GameEngine/GameEngineComponent";
+import "./MainPage.css";
+import { Link } from "react-router-dom";
+
+function MainPage() {
+    return (
+        <div className="mainPageContainer">
+            <div className="mainPageHeader">
+                <text className="mainPageHeaderName">Team Fitlez</text>
+            </div>
+            <div class="mainPageBody">
+                <div class="leftBody">
+                    <h2 className="leftBodyHeader">Singleplayer</h2>
+                    <h5 className="leftBodySubHeader">Try it out, now!</h5>
+                    <Link to="/game" class="button">
+                        <span>START A GAME</span>
+                    </Link>
+                </div>
+                <div class="rightBody">
+                    <h2 className="leftBodyHeader">Multiplayer</h2>
+                    <h5 className="leftBodySubHeader">Try it out, now!</h5>
+                    <div className="rightBodyButtons">
+                        <button class="button">
+                            <span>CREATE A GAME</span>
+                        </button>
+                        <button class="button">
+                            <span>JOIN A GAME</span>
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default MainPage;
--- src/index.js
+++ src/index.js
@@ -2,16 +2,24 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.scss";
 import "handsfree/build/lib/assets/handsfree.css";
-import App from "./App";
 import Handsfree from "handsfree";
+import MainPage from "./MainPage/MainPage";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { GameEngineComponent } from "./GameEngine/GameEngineComponent";
 
 window.handsfree = new Handsfree({ pose: true, showDebug: true });
 window.handsfree.enablePlugins("browser");
 window.handsfree.start();
 
 ReactDOM.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>,
+    <Router>
+        <React.StrictMode>
+            <MainPage />
+        </React.StrictMode>
+        <Routes>
+            {/* <Route exact path="/" element={<MainPage />} /> */}
+            <Route exact path="/game" element={<GameEngineComponent />} />
+        </Routes>
+    </Router>,
     document.getElementById("root")
 );
