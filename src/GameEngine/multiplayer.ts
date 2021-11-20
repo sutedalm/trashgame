af2a95d147f0ed1441e19c10fd73986b110f700b
sutedalm <37000551+sutedalm@users.noreply.github.com>
2021-11-20 17:33:07 +0100

    update multiplayer server id loading

--- src/GameEngine/multiplayer.ts
+++ src/GameEngine/multiplayer.ts
@@ -1,11 +1,22 @@
 
+import { useState } from "react";
 import socketIOClient from "socket.io-client";
 
 const ENDPOINT = "https://sheltered-forest-46021.herokuapp.com/";
 
-const multiplayer = socketIOClient(ENDPOINT);
 
-export default multiplayer;
+const socket = socketIOClient(ENDPOINT);
+
+export function useMultiplayerId() {
+  const [id, setId] = useState(socket.id)
+
+  socket.on("connect", () => { setId(socket.id) });
+
+
+  return id
+}
+
+export default socket;
 
 // socket.on("chat message", function (msg) {
 //   console.log("received: ", msg);
--- src/components/CopyClipboardButton/CopyClipboardButton.tsx
+++ src/components/CopyClipboardButton/CopyClipboardButton.tsx
@@ -1,4 +1,4 @@
-import multiplayer from "../../GameEngine/multiplayer";
+import multiplayer, { useMultiplayerId } from "../../GameEngine/multiplayer";
 import { useEffect, useState } from "react";
 import { CopyToClipboard } from "react-copy-to-clipboard";
 import clipboardIcon from "./icons8-kopieren-24.png";
@@ -6,23 +6,21 @@ import clipboardIcon from "./icons8-kopieren-24.png";
 import "./CopyClipboardButton.scss";
 
 const CopyClipboardButton = () => {
-    const [gameRoomId, setGameRoomId] = useState(multiplayer.id);
-    useEffect(() => {
-        setGameRoomId(multiplayer.id);
-    }, [multiplayer.id]);
+    const gameRoomId = useMultiplayerId();
 
     const [copied, setCopied] = useState(false);
 
-    const [gameRoomIdCopied, setGameRoomIdCopied] = useState(false);
     return (
         <div className="CopyClipboardButon__container">
-            {gameRoomId && (
+            {gameRoomId ? (
                 <CopyToClipboard text={gameRoomId} onCopy={() => setCopied(true)}>
                     <button className="CopyClipboardButon__button">
                         <span>{gameRoomId}</span>
                         <img src={clipboardIcon} alt="copy" />
                     </button>
                 </CopyToClipboard>
+            ) : (
+                <span>connecting to server...</span>
             )}
             {copied && <span>Copied!</span>}
         </div>
