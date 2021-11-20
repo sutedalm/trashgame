1456b729087661c9fa75f0c10fba1b6d5829b903
sutedalm <37000551+sutedalm@users.noreply.github.com>
2021-11-20 03:54:19 +0100

    initial handsfree app

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import "./App.css";
+// Inside your app
+
+function App() {
+  return <div className="App"></div>;
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import "handsfree/build/lib/assets/handsfree.css";
+import App from "./App";
+import Handsfree from "handsfree";
+
+window.handsfree = new Handsfree({ pose: true, showDebug: true });
+window.handsfree.enablePlugins("browser");
+window.handsfree.start();
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
